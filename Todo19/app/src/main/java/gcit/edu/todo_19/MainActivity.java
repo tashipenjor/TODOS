8fc6f79abee527da405baf4dea7cf02ebd460fc6
DhendupGhishing <dhendupghishing@gmail.com>
2021-05-22 23:25:26 +0600

    Todo_18_19

---
+++ Todo19/app/src/main/java/gcit/edu/todo_19/MainActivity.java
@@ -0,0 +1,56 @@
+package gcit.edu.todo_19;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+
+import gcit.edu.todo_19.R;
+
+import java.util.ArrayList;
+
+import gcit.edu.todo_19.Sports;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private ArrayList<Sports> mSportsData;
+    private SportsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int gridColumnCount=getResources().getInteger(R.integer.grid_column_count);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+      mRecyclerView.setLayoutManager(new GridLayoutManager(this,gridColumnCount));
+
+
+        mSportsData = new ArrayList<>();
+
+        mAdapter = new SportsAdapter(this,mSportsData);
+        mRecyclerView.setAdapter(mAdapter);
+
+        initializeData();
+    }
+
+    private void initializeData() {
+        TypedArray sportsImageResources = getResources().obtainTypedArray(R.array.sports_img);
+        String[] sportsList = getResources().getStringArray(R.array.sports_titles);
+        String[] sportsInfo = getResources().getStringArray(R.array.sports_info);
+
+        mSportsData.clear();
+
+        for(int i=0; i<sportsList.length;i++){
+            mSportsData.add(new Sports(sportsList[i],sportsInfo[i],sportsImageResources.getResourceId(i,0)));
+        }
+        mAdapter.notifyDataSetChanged();
+        sportsImageResources.recycle();
+    }
+}
